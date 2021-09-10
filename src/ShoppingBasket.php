d8109f8e0aa6376d24fc96619bbade73d558dcbc
Jakub Gadkowski <jakubgg@gmail.com>
2021-09-10 15:49:25 +0100

    adds docblocks for shoppingbasket

--- src/ShoppingBasket.php
+++ src/ShoppingBasket.php
@@ -8,11 +8,17 @@ use Basket\Shipping\Shipping;
 
 class ShoppingBasket
 {
+    /**
+     * Holds codes of items added,
+     * these are used to count value of basket.
+     */
     private array $items_in_basket = [];
+
+    /**
+     * Temporary basket for counting prices,
+     * shipping, applying offers.
+     */
     private array $transient_basket = [];
-
-    private ?int $shipping_total = null; //? not necessary
-
 
     public function __construct(
         protected ?array $product_catalogue = [],
@@ -22,7 +28,15 @@ class ShoppingBasket
     }
 
     /**
-     * Allows Shopping Basket initialisation at a later time, and it's easier for testing.
+     * Allows Shopping Basket initialisation at a later time,
+     * looks better than passing all arguments to the constructor and it's easier for testing.
+     * Can be chained.
+     *
+     * @param ?array $product_catalogue An array of products with name, product code and price.
+     * @param ?Shipping $shipping_rates An object of type 'Shipping', provides rules for shipping charges.
+     * @param ?Offer $offers An object of type 'Offer', provides rules for applying Offers.
+     *
+     * @return ShoppingBasket
      */
     public function init($product_catalogue, $shipping_rates, $offers): ShoppingBasket
     {
@@ -35,6 +49,11 @@ class ShoppingBasket
         return $this;
     }
 
+    /**
+     * Create temporary basket with product codes and prices
+     *
+     * @return void
+     */
     protected function fillTransientBasket(): void
     {
         $this->transient_basket = [];
@@ -50,6 +69,11 @@ class ShoppingBasket
         }
     }
 
+    /**
+     * Calculates total value of items in transient basket
+     *
+     * @return int Value of all items in transient basket in cents.
+     */
     protected function getTransientBasketValue(): int
     {
         $basket_value = 0;
@@ -61,6 +85,13 @@ class ShoppingBasket
         return $basket_value;
     }
 
+    /**
+     * Searches through 'products array' by 'product code'
+     * and checks if the item (item code) in basket has a match there so later on we can obtain its price.
+     *
+     * @return int|Exception Returns integer pointing to array index of the item in product catalogue on success,
+     * or raises Exception if the item does not exist in the catalogue.
+     */
     protected function searchItemInBasket($item_code): int | Exception
     {
         $index = array_search($item_code, array_column($this->product_catalogue, 'code'));
@@ -71,6 +102,12 @@ class ShoppingBasket
         return $index;
     }
     
+    /**
+     * Checks if the Basket class has been initialised with all arguments necessary for calculating totals.
+     *
+     * @return bool | Exception Returns `true` on success,
+     * or raises Exception if there is something missing.
+     */
     protected function checkSetupValidity() : bool | Exception
     {
         if (empty($this->product_catalogue) || is_null($this->shipping_rates)) {
@@ -80,12 +117,25 @@ class ShoppingBasket
         return true;
     }
 
+    /**
+     * Adds item to the items_in_basket.
+     * Can be chained.
+     *
+     * @return ShoppingBasket
+     */
     public function add(string $product_code): ShoppingBasket
     {
         array_push($this->items_in_basket, $product_code);
         return $this;
     }
     
+    /**
+     * Gets the total value of items_in_basket and
+     * hands over to 'Shipping' object to calculate shipping price.
+     * Must be last in the chain of commands.
+     *
+     * @return int Value of shopping for items in cents.
+     */
     public function getDelivery(): int
     {
         $this->fillTransientBasket();
@@ -93,17 +143,37 @@ class ShoppingBasket
         return $shipping;
     }
 
+    /**
+     * Recalculates transient basket and
+     * obtains total value of items in it.
+     * Must be last in the chain of commands.
+     *
+     * @return int Value of all items in transient basket in cents.
+     */
     public function getBasketValue(): int
     {
         $this->fillTransientBasket();
         return $this->getTransientBasketValue();
     }
 
+    /**
+     * Returns items_in_basket
+     * Must be last in the chain of commands.
+     *
+     * @return array
+     */
     public function getBasket(): array
     {
         return $this->items_in_basket;
     }
 
+    /**
+     * Gathers all parts, smashes them together and returns grand total with offers and shipping applied.
+     * Allows to inject `Offer` at the last moment, if the offer was ommited from init().
+     * Must be last in the chain of commands.
+     *
+     * @return int Value of all items with Shipping and Offer applied, in cents.
+     */
     public function getTotal(?Offer $offer = null): int | Exception
     {
         if (is_null($this->offers) && is_null($offer)) {
