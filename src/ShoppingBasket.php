0c8c0e448f4fc1a0b394bf72937fe006aca10b90
Jakub Gadkowski <jakubgg@gmail.com>
2021-09-10 11:45:15 +0100

    adds more tests and working basket

--- src/ShoppingBasket.php
+++ src/ShoppingBasket.php
@@ -3,36 +3,26 @@
 namespace Basket;
 
 use Exception;
+use Basket\Offers\Offer;
+use Basket\Shipping\Shipping;
 
 class ShoppingBasket
 {
-    private static ?ShoppingBasket $instance = null;
     private array $items_in_basket = [];
+    private array $transient_basket = [];
+
+    private ?int $shipping_total = null; //? not necessary
 
 
-    /**
-     * Avoids multiple Shopping baskets during the lifecycle of the script
-     */
-    public static function getInstance(): ShoppingBasket
-    {
-        if (self::$instance === null) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
-    }
-
-    private function __construct(
+    public function __construct(
         protected ?array $product_catalogue = [],
-        protected ?object $shipping_rates = null,
-        protected ?object $offers = null,
+        protected ?Shipping $shipping_rates = null,
+        protected ?Offer $offers = null,
     ) {
     }
-  
-        /**
-     * Initialising the basket via getInstance looks ugly,
-     * so instead we have a method to initialse the basket.
-     * This also allow us to init basket later in the process if necessary
+
+    /**
+     * Allows Shopping Basket initialisation at a later time, and it's easier for testing.
      */
     public function init($product_catalogue, $shipping_rates, $offers): ShoppingBasket
     {
@@ -40,12 +30,47 @@ class ShoppingBasket
         $this->shipping_rates = $shipping_rates ?? null;
         $this->offers = $offers ?? null;
         
-        $this->checkBasketValidity();
+        $this->checkSetupValidity();
 
         return $this;
     }
 
-    protected function checkBasketValidity() : bool | Exception
+    protected function fillTransientBasket(): void
+    {
+        try {
+            array_map(function ($item_code) {
+                $index = $this->searchItemInBasket($item_code);
+                $this->transient_basket[] = [
+                    $this->product_catalogue[$index]['code'] => $this->product_catalogue[$index]['price']
+                ];
+            }, $this->items_in_basket);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected function getTransientBasketValue(): int
+    {
+        $basket_value = 0;
+
+        array_walk_recursive($this->transient_basket, function ($value, $code) use (&$basket_value) {
+            $basket_value += $value;
+        });
+
+        return $basket_value;
+    }
+
+    protected function searchItemInBasket($item_code): int | Exception
+    {
+        $index = array_search($item_code, array_column($this->product_catalogue, 'code'));
+        if ($index === false) {
+            throw new Exception('Product code not found in the products_catalogue.');
+        }
+
+        return $index;
+    }
+    
+    protected function checkSetupValidity() : bool | Exception
     {
         if (empty($this->product_catalogue) || is_null($this->shipping_rates)) {
             throw new Exception('Basket not properly initialised. Missing Products/Shipping rates');
@@ -60,14 +85,43 @@ class ShoppingBasket
         return $this;
     }
     
+    public function getDelivery(): int
+    {
+        $shipping = $this->shipping_rates->getShipping($this->getTransientBasketValue());
+        return $shipping;
+    }
+
+    public function getBasketValue(): int
+    {
+        $this->fillTransientBasket();
+        return $this->getTransientBasketValue();
+    }
+
     public function getBasket(): array
     {
         return $this->items_in_basket;
     }
-    
+
     public function getTotal(): int
     {
-        // TODO
-        return 0;
+        $this->checkSetupValidity();
+        $this->fillTransientBasket();
+
+        return $this->getTransientBasketValue();
+    }
+
+    public function getTotalWithOffer(?Offer $offer = null): int | Exception
+    {
+        if (is_null($this->offers) && is_null($offer)) {
+            throw new Exception('No offers provided');
+        }
+
+        $this->checkSetupValidity();
+        $this->fillTransientBasket();
+        $this->transient_basket = $this->offers->applyOffer($this->transient_basket);
+        $basket_value = $this->getTransientBasketValue();
+        $delivery = $this->getDelivery();
+        
+        return $basket_value + $delivery;
     }
 }
--- tests/ShoppingBasketTest.php
+++ tests/ShoppingBasketTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 namespace Tests;
 
+use Basket\Shipping\StandardShipping;
 use Basket\ShoppingBasket;
 use Exception;
 use PHPUnit\Framework\TestCase;
@@ -27,50 +28,96 @@ final class ShoppingBasketTest extends TestCase
             'price' => 2495,
         ],
     ];
+    
+    private $test_product = [
+        [
+            'product' =>'Red Widget',
+            'code' =>'R01',
+            'price' => 2500,
+        ],
+    ];
 
     protected function setUp(): void
     {
-
-        $this->shopping_basket = ShoppingBasket::getInstance();
+        $this->shopping_basket = new ShoppingBasket();
     }
 
-    public function testShoppingBasketUniqueness()
+    protected function tearDown(): void
     {
-        $firstCall = ShoppingBasket::getInstance();
-        $secondCall = ShoppingBasket::getInstance();
-
-        $this->assertInstanceOf(ShoppingBasket::class, $firstCall);
-        $this->assertSame($firstCall, $secondCall);
     }
 
     public function testAddMethodReturnsInstanceOfShoppingBasket(): void
     {
+        $this->shopping_basket = new ShoppingBasket();
         $this->assertInstanceOf(ShoppingBasket::class, $this->shopping_basket->add($this->valid_basket_value));
     }
 
     public function testAddMethodAddsToItemsInBasket(): void
     {
+        $this->shopping_basket->add($this->valid_basket_value);
         $this->assertContains($this->valid_basket_value, $this->shopping_basket->getBasket());
+    }
+
+    public function testBasketDetectsIllegalItem(): void
+    {
+        $this->expectExceptionMessage('Product code not found in the products_catalogue.');
+        $this->shopping_basket->add($this->invalid_basket_value)->getBasketValue();
+    }
+
+    public function testBasketValueReturnsCorrectValueWithChaining(): void
+    {
+        $shipping_stub = $this->createStub(\Basket\Shipping\Shipping::class);
+        $this->shopping_basket->init(product_catalogue: $this->products, shipping_rates: $shipping_stub, offers: null);
+        $this->assertEquals(0, $this->shopping_basket->getBasketValue());
+        $this->assertEquals(4090, $this->shopping_basket->add('R01')->add('B01')->getBasketValue());
+    }
+
+    public function testBasketValueReturnsCorrectValue(): void
+    {
+        $shipping_stub = $this->createStub(\Basket\Shipping\Shipping::class);
+        $this->shopping_basket->init(product_catalogue: $this->products, shipping_rates: $shipping_stub, offers: null);
+        $this->assertEquals(0, $this->shopping_basket->getBasketValue());
+        $this->assertEquals(3295, $this->shopping_basket->add('R01')->getBasketValue());
+        $this->assertEquals(4090, $this->shopping_basket->add('R01')->add('B01')->getBasketValue());
+        $this->assertEquals(6585, $this->shopping_basket->add('G01')->getBasketValue());
     }
 
     public function testInitFailedToSet(): void
     {
-        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Basket not properly initialised. Missing Products/Shipping rates');
         $this->shopping_basket->init(product_catalogue: null, shipping_rates: null, offers: null);
     }
 
-    public function testInitProperlySet(): void
+    public function testInitSetSuccesfully(): void
     {
         $shipping_stub = $this->createStub(\Basket\Shipping\Shipping::class);
         $offer_stub = $this->createStub(\Basket\Offers\Offer::class);
 
-        $this->assertInstanceOf(ShoppingBasket::class, $this->shopping_basket->init(product_catalogue: $this->products, shipping_rates: $shipping_stub, offers: $offer_stub));
+        $this->assertInstanceOf(
+            ShoppingBasket::class,
+            $this->shopping_basket->init(
+                product_catalogue: $this->products,
+                shipping_rates: $shipping_stub,
+                offers: $offer_stub
+            )
+        );
     }
 
+    public function testItemsBasketReturnedCorrectly(): void
+    {
+        $this->shopping_basket->add('R01');
+        $this->shopping_basket->add('B01');
+        $this->shopping_basket->add('G01');
+        $this->assertEquals(['R01','B01','G01'], $this->shopping_basket->getBasket());
+    }
 
+    public function testShippingRatesBelow50(): void
+    {
+        $this->shopping_basket->init(product_catalogue: $this->test_product, shipping_rates: new StandardShipping, offers: null);
+        $this->shopping_basket->add('R01');
+        $this->assertEquals(495, $this->shopping_basket->getDelivery());
+    }
     // getDelivery
-    // getBasketValue
-    // getBasket
     // getTotal
     // getTotalWithOffer
 }
